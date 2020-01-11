95035ca14d87339711647ebbe7c5d80e7e9ba4d5
Jonathan Roth <rothje@vcu.edu>
2020-01-11 14:01:38 -0500

    added footer

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+<div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>MUSIC PLAYER HERE</p>
+      <footer class="blockquote-footer">OR FRIGGIN' HERE!</footer>
+    </blockquote>
+  </div>
+);
+
+export default Footer;
