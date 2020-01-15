1d4a26a38c16189a4c4e7d33634471a6c97b749d
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-14 22:02:30 -0500

    modified stlyinng and color matched every element with palette

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import "./Dashboard.css";
+
 
 class Dashboard extends Component {
   onLogoutClick = e => {
@@ -18,9 +20,15 @@ class Dashboard extends Component {
           <div className="landing-copy col s12 center-align">
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
+              <div class="card">
+                <div class="card-body">
               <p className="flow-text grey-text text-darken-1">
                 What would you like to do today? 
               </p>
+              <p className="flow-text grey-text text-darken-1">Today is today</p>
+              <p className="flow-text grey-text text-darken-1">The weather for today is whatever it looks and feels like outside</p>
+              </div>
+             </div>
             </h4>
             <button
               style={{
@@ -29,8 +37,23 @@ class Dashboard extends Component {
                 letterSpacing: "1.5px",
                 marginTop: "1rem"
               }}
+              //onClick={this.onLogoutClick}
+              id="gameswitch"
+              className="btn btn-large waves-effect waves-light hoverable accent-3"
+            >
+              Lets play!
+            </button>
+            <br></br>
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
               onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              id="logout"
+              className="btn btn-large waves-effect waves-light hoverable accent-3"
             >
               Logout
             </button>
