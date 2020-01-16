9569ea87cb47d93634a3bb4a8d25600be7c8bd53
Jonathan Roth <rothje@vcu.edu>
2020-01-15 20:22:13 -0500

    added MusicPlayer component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./layout.css"
 
 
 class Navbar extends Component {
   render() {
+    
     return (
+      
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
+          <div className="nav-wrapper blue">
+
             <Link
               to="/"
               style={{
                 fontFamily: "Lucida Sans Typewriter"
               }}
-              className="col s5 brand-logo center black-text"
+              // className="col s5 brand-logo black-text"
             >
-              <i className="material-icons"></i>
-              Moneta
+        <img
+        id="logo"
+        src={require('./logo.png')}
+        width="100"
+        height="100"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+        href="/"
+      />
             </Link>
           </div>
         </nav>
       </div>
+      
     );
   }
 }
