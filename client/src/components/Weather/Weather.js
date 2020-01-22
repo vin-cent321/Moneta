dc3cdfacab1983ea6fa62dde3f5765eba203749d
Frederick Articuno <mckiyoshi@gmail.com>
2020-01-22 15:51:20 -0500

    modified temp

--- client/src/components/Weather/Weather.js
+++ client/src/components/Weather/Weather.js
@@ -33,8 +33,8 @@ function weather() {
         $(".temp").text("Temperature (F) " + response.main.temp);
 
         // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
+        console.log("Wind Speed: " + response.wind.speed+"mph");
+        console.log("Humidity: " + response.main.humidity+"%");
         console.log("Temperature (F): " + response.main.temp);
       });
 }
