3fe8a3288a67d0b08fcab150629348fb621f140c
Frederick Articuno <mckiyoshi@gmail.com>
2020-01-24 12:40:57 -0500

    game now tells you why you're wrong, opacity and border still broken

--- client/src/components/MatchIt/MatchIt.js
+++ client/src/components/MatchIt/MatchIt.js
@@ -8,9 +8,10 @@ class MatchIt extends Component {
         data,
         score: 0,
         hiddenAnswer: [],
-        disabled: false
+        disabled: false,
+        message:'',
+        name:'',
     }
-
 
 componentDidMount() {
     this.shuffleData(this.state.data);
@@ -26,15 +27,20 @@ selectHiddenAnswer() {
 
 correctGuess = newData => {
     const { score } = this.state;
-    this.setState({ score: score + 1 });
+    this.setState({ 
+        score: score + 1,
+        message: `Yes this is ${this.state.hiddenAnswer.name}`} );
     // update score, message
 };
 
 incorrectGuess = data => {
     const { score } = this.state;
-    this.setState({ score: score + 0 });
+    let e = this // What defines the image just clicked? this?
+    this.setState({ 
+        score: score + 0, 
+        message: `No this is ${this.state.name}`});
+        // update score, message
 };
-
 resetData = data => {
     // const resetData = data.map(item => ({ ...item, match: true}));
     return this.shuffleData(this.state.data);
@@ -47,24 +53,31 @@ shuffleData = data => {
         const temporary = data[i];
         data[i] = data[j];
         data[j] = temporary;
+        this.setState({
+            name: data[i].name
+        })
         i--;
     }
     return data;
 };
 
 clickHandler = id => {
+    
     if (this.state.hiddenAnswer.id === id) {
+        
         this.correctGuess()
 
     } else {
         this.incorrectGuess();
 
     }
+    console.log(this.state.name)
     this.setState({ disabled: true})
     setTimeout(() => {
         this.shuffleData(this.state.data);
         this.selectHiddenAnswer();
-        this.setState({ disabled: false })
+        this.setState({ disabled: false,
+        message:'' })
     }, 3000);
 };
 
@@ -72,17 +85,19 @@ render() {
     return (
         <div><br></br>
             <Container>
-                <div class="black-text">Click The Picture of {this.state.hiddenAnswer.name}!</div>
+                <div className="black-text">Click The Picture of {this.state.hiddenAnswer.name}! {this.state.message}</div>
+                
             </Container>
             <Container>    
-                <div class="black-text">Score: {this.state.score}</div>
+                <div className="black-text">Score: {this.state.score}</div>
             </Container>
             <Container>
                 {this.state.data.map(item => (
                     <Click
                     key={item.id}
                     id={item.id}
-                    fade={!this.state.score}
+                    opacity={1}
+                    name={item.name}
                     handleClick={this.state.disabled ? () => {} : this.clickHandler}
                     image={item.image}
                     />
