7e159a24beadd2952633d87438d62c947f423033
clarymt <52682135+clarymt@users.noreply.github.com>
2020-01-23 19:26:15 -0500

    doing things

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { logoutUser } from "../../actions/authActions";
 import "./Dashboard.css";
 import Weather from "../Weather/Weather";
 import Moment from "react-moment";
+import { Link } from "react-router-dom";
+
  
 
 
@@ -28,7 +30,7 @@ class Dashboard extends Component {
               <div class="card">
                 <div class="card-body">
               <p className="flow-text grey-text text-darken-1">
-                Ready to play? 
+                What would you like to do today? 
               </p>
               <p className="flow-text grey-text text-darken-1">Today is today</p>
             <Moment local></Moment>
@@ -37,19 +39,19 @@ class Dashboard extends Component {
               </div>
              </div>
             </h4>
-            <a href="/mood" role="button"
+            <Link to="/mood" role="button"
               style={{
                 width: "150px",
                 borderRadius: "3px",
                 letterSpacing: "1.5px",
                 marginTop: "1rem"
               }}
-              onClick={"./mood/mood"}
+              //onClick={"./mood/mood"}
               //id=""
               className="gameswitch btn btn-large waves-effect waves-light hoverable accent-3"
             >
               Lets play!
-            </a>
+            </Link>
             <br></br>
             <button
               style={{
